5600e1ca8d25218338580a52089fa81c78a5e6c4
Zhabskyi <poltavagaz@gmail.com>
2024-05-08 13:36:59 -0600

    ticket: feature(dependencies) uninstall react-quary due to no need

--- client/dashboard/src/components/base/Root/Root.tsx
+++ client/dashboard/src/components/base/Root/Root.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./root.css";
-import { QueryClient } from "@tanstack/react-query";
 import { StyledEngineProvider } from "@mui/material/styles";
 import App from "../App";
-
-const queryClient = new QueryClient();
 
 const Root: React.FC = () => {
   return (

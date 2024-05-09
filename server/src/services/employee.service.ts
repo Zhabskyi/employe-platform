10658130d6932dc29a449359ccfec7058457a126
Zhabskyi <poltavagaz@gmail.com>
2024-05-08 18:22:04 -0600

    ticket: feature(server) create service getAllEmployees

--- server/src/services/employee.service.ts
+++ server/src/services/employee.service.ts
@@ -1,4 +1,12 @@
+import Helper from '@/db/helper';
 import { Service } from 'typedi';
 
 @Service()
-export class EmployeeService {}
+export class EmployeeService {
+  public async getAllEmployees(): Promise<any> {
+    const sql = `SELECT * FROM employees`;
+    const pool = Helper.pool();
+    const query_results = await pool.query(sql);
+    return query_results.rows;
+  }
+}

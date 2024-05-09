65d09ca836c72a25011ad5b7c3614b72dad84347
Zhabskyi <poltavagaz@gmail.com>
2024-05-09 15:29:33 -0600

    ticket12: feature(Form) create adit and delete buttons

--- client/employee-app/src/components/pages/Home/Table/CellRenderers/ActionButtons.tsx
+++ client/employee-app/src/components/pages/Home/Table/CellRenderers/ActionButtons.tsx
@@ -1,7 +1,42 @@
-const ActionButtons: React.FC = () => {
-  // Implement the component logic here
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Grid } from "@mui/material";
+import * as S from "./ActionButtons.css";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "../../../../../utilities/constants";
+import { IEmployee } from "../../../../../models/Employees/Employees";
 
-  return <div>{/* Render the action buttons here */}</div>;
+interface Props {
+  data: IEmployee;
+}
+
+const ActionButtons: React.FC = ({ data }: Props) => {
+  const values = { ...data };
+  const navigate = useNavigate();
+
+  const onEditClick = () => {
+    navigate(
+      `/${PATHS.HOME}/${PATHS.EDIT_EMPLOYEE}?
+      firstName=${values.firstName}&
+      lastName=${values.lastName}&
+      department=${values.department}&
+      salary=${values.salary}`
+    );
+  };
+  return (
+    <Grid container justifyContent="center" alignItems="center" height={"100%"}>
+      <Grid item xs={6}>
+        <S.StyledButton variant="outlined" color="info" onClick={onEditClick}>
+          <EditIcon color="action" />
+        </S.StyledButton>
+      </Grid>
+      <Grid item xs={6}>
+        <S.StyledButton variant="outlined" color="info">
+          <DeleteIcon color="error" />
+        </S.StyledButton>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ActionButtons;

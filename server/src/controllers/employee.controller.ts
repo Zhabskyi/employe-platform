47a4e1500cdfec6f69713296717d7cb79c15fe2e
Zhabskyi <poltavagaz@gmail.com>
2024-05-08 18:24:43 -0600

    ticket: feature(server) add getEmployees controller

--- server/src/controllers/employee.controller.ts
+++ server/src/controllers/employee.controller.ts
@@ -1,5 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import { Container } from 'typedi';
 import { Employee } from '@/interfaces/employee.interface';
+import { EmployeeService } from '@/services/employee.service';
 
-export class EmployeeController {}
+export class EmployeeController {
+  public employee = Container.get(EmployeeService);
+
+  public getEmployees = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const getAllPosts: Employee[] = await this.employee.getAllEmployees();
+
+      res.status(200).json({ data: getAllPosts, message: 'success' });
+    } catch (error) {
+      next(error);
+    }
+  };
+}

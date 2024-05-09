691fd03840268927635a445d1517bde4878b48c5
Zhabskyi <poltavagaz@gmail.com>
2024-05-08 18:39:39 -0600

    ticket: feature(server): finish employee route

--- server/src/routes/employees.route.ts
+++ server/src/routes/employees.route.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 import { Routes } from '@interfaces/routes.interface';
+import { EmployeeController } from '@/controllers/employee.controller';
 
-export class PostRoute implements Routes {
-  public path = '/posts';
+export class EmployeeRoute implements Routes {
+  public path = '/employees';
   public router: Router = Router();
+  public employee = new EmployeeController();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`);
+    this.router.get(`${this.path}`, this.employee.getEmployees);
     this.router.post(`${this.path}`);
     this.router.put(`${this.path}/:id`);
     this.router.delete(`${this.path}/:id`);

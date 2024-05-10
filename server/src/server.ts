30c77ecae676edc2969c7bd5778dfd545a213032
Zhabskyi <poltavagaz@gmail.com>
2024-05-09 19:39:03 -0600

    ticket13: feature(server) add department route to App

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 import { App } from './app';
+import { DepartmentRoute } from './routes/department.route';
 import { ValidateEnv } from './utilities/validateEnv';
 import { EmployeeRoute } from '@routes/employees.route';
 
 ValidateEnv();
 
-const app = new App([new EmployeeRoute()]);
+const app = new App([new EmployeeRoute(), new DepartmentRoute()]);
 
 app.listen();
